5c7de30c32810634eaf61ab535517191a4bd6d68
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-04-06 23:34:44 +0300

    add changes for autoDetails css

--- src/pages/AutoDetails/AutoDetails.jsx
+++ src/pages/AutoDetails/AutoDetails.jsx
@@ -6,6 +6,7 @@ import CarInfo from "../../components/CarInfo/CarInfo.jsx";
 import { fetchCarById } from "../../redux/cars/operations.js";
 import Loader from "../../components/Loader/Loader.jsx";
 import BookForm from "../../components/BookForm/BookForm.jsx";
+import s from "./AutoDetails.module.css";
 
 const AutoDetails = () => {
   const { id } = useParams();
@@ -18,11 +19,11 @@ const AutoDetails = () => {
   }, [dispatch, id]);
 
   return (
-    <div>
+    <div className="container">
       {loading || !car.id ? (
         <Loader />
       ) : (
-        <section>
+        <section className={s.detailsWrap}>
           <div>
             <img src={car.img} alt={car.description} width="640" />
             <BookForm />
